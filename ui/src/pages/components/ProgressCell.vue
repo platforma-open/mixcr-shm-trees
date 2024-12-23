9e8bbc975f0e3640bf2390456a6cfa7c6c9bd1f7
nikolaygiman <gimanhead@gmail.com>
2024-12-23 13:15:16 +0100

    fix: added PlAgCellProgress

--- ui/src/pages/components/ProgressCell.vue
+++ ui/src/pages/components/ProgressCell.vue
@@ -3,6 +3,8 @@ import { ICellRendererParams } from 'ag-grid-enterprise';
 import { ProgressPattern } from '@platforma-open/milaboratories.mixcr-shm-trees.model';
 import { computed, unref } from 'vue';
 import { TreeResult } from '../../results';
+import type { PlProgressCellProps } from '@platforma-sdk/ui-vue';
+import { PlAgCellProgress } from '@platforma-sdk/ui-vue';
 
 const props = defineProps<{
   params: ICellRendererParams<TreeResult, string>;
@@ -58,67 +60,16 @@ const parsed = computed<Parsed>(() => {
   return res;
 });
 
-const canShowBackground = computed(
-  () => parsed.value.stage !== 'Queued' && (parsed.value.etaLabel ?? parsed.value.percentageLabel)
-);
+const ProgressProps = computed<PlProgressCellProps>(() => {
+  return {
+    stage: parsed.value.raw === 'Queued' ? 'not_started' : 'running',
+    step: parsed.value.stage || '',
+    progress: parsed.value.percentage ? +parsed.value.percentage : undefined,
+    progressString: parsed.value.etaLabel || ''
+  };
+});
 </script>
 
 <template>
-  <div :class="{ 'progress-cell__white-bg': canShowBackground }" class="progress-cell">
-    <div class="progress-cell__indicator" :style="{ width: parsed.percentage + '%' }"></div>
-    <div class="progress-cell__body">
-      <div :class="{ 'progress-cell__stage--queued': parsed.stage === 'Queued' }" class="progress-cell__stage">
-        {{ parsed.stage }}
-      </div>
-      <div class="progress-cell__percentage">{{ parsed.etaLabel ?? parsed.percentageLabel }}</div>
-    </div>
-  </div>
+  <PlAgCellProgress v-bind="{ params: { ...props.params, ...ProgressProps } }" />
 </template>
-
-<style lang="css">
-.progress-cell {
-  height: 100%;
-  position: relative;
-  width: 100%;
-  overflow: hidden;
-  border-radius: 2px;
-}
-
-.progress-cell__white-bg {
-  background-color: #fff;
-}
-
-.progress-cell__indicator {
-  position: absolute;
-  height: 100%;
-  transition: width 0.4s ease-in-out;
-  background: linear-gradient(90deg, #fff 0%, #d8fac8 100%);
-}
-
-.progress-cell__body {
-  padding: 0 15px;
-  display: flex;
-  gap: 12px;
-  align-items: center;
-  height: 100%;
-  width: 100%;
-  position: absolute;
-  z-index: 1;
-}
-
-.progress-cell__stage {
-  overflow: hidden;
-  text-overflow: ellipsis;
-  flex-shrink: 1;
-}
-
-.progress-cell__percentage {
-  flex-grow: 1;
-  flex-shrink: 0;
-  text-align: right;
-}
-
-.progress-cell__stage--queued {
-  color: var(--txt-03);
-}
-</style>
